16265d47fd0708e8038ba7db5a51e0a9abbc7d62
Chen <chen.guoxin@trans-cosmos.com.cn>
2019-12-09 17:50:06 +0800

    add BatchAction

---
+++ src/main/java/com/iohgame/framework/utility/BatchAction.java
@@ -0,0 +1,18 @@
+package com.iohgame.framework.utility;
+
+import com.iohgame.framework.connect.base.ConnectBase;
+
+public abstract class BatchAction<T extends ConnectBase> extends ServiceAction
+{
+    private T m_connect;
+
+    public BatchAction(T connect)
+    {
+        m_connect = connect;
+    }
+
+    protected T connect()
+    {
+        return m_connect;
+    }
+}
